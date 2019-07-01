3e1234277ef91e7a9983e47053d5eb0805a5f016
Adrienne <adrienneeaston@gmail.com>
2019-06-30 19:02:59 -0700

    math functions and tests done except acounting for division by zero

--- __tests__/arithmetic.test.js
+++ __tests__/arithmetic.test.js
@@ -6,20 +6,76 @@ const faker = require('faker');
 const randomNum = faker.random.number();
 
 describe('addition tests', () => {
-  it('should work as expected', () => {
+  it('should add numbers as expected', () => {
     let num1 = randomNum;
     let num2 = randomNum;
     let answer = num1 + num2;
     let results = arithmetic.add(num1, num2);
     expect(results).toEqual(answer);
   });
-  it('should not work if either param is not a number', () =>{
+
+  it('should not work if either param is not a number', () => {
     let num1 = 'banana';
     let num2 = randomNum;
     let results = arithmetic.add(num1, num2);
     expect(results).toBeNull(); 
-
-    // if(typeof num1 !== "number" || typeof num2 !== "number") {
-    //   return null;
   });
+})
+
+describe('subtraction tests', () => {
+  it('should subtract numbers as expected', () => {
+    let num1 = randomNum;
+    let num2 = randomNum;
+    let answer = num1 - num2;
+    let results = arithmetic.subtract(num1, num2);
+    expect(results).toEqual(answer);
+  });
+
+  it('should not work if either param is not a number', () => {
+    let num1 = 'apple';
+    let num2 = randomNum;
+    let results = arithmetic.subtract(num1, num2);
+    expect(results).toBeNull();
+  });
+})
+
+describe('multiply tests', () => {
+  it('should multiple numbers as expected', () => {
+    let num1 = randomNum;
+    let num2 = randomNum;
+    let answer = num1 * num2;
+    let results = arithmetic.multiply(num1, num2);
+    expect(results).toEqual(answer);
+  });
+
+  it('should not work if either param is not a number', () => {
+    let num1 = 'blueberry';
+    let num2 = randomNum;
+    let results = arithmetic.multiply(num1, num2);
+    expect(results).toBeNull();
+  });
+})
+
+describe('division tests', () => {
+  it('should divide numbers as expected', () => {
+    let num1 = randomNum;
+    let num2 = randomNum;
+    let answer = num1 / num2;
+    let results = arithmetic.divide(num1, num2);
+    expect(results).toEqual(answer);
+  });
+
+  it('should not work if either param is not a number', () => {
+    let num1 = 'mango';
+    let num2 = randomNum;
+    let results = arithmetic.divide(num1, num2);
+    expect(results).toBeNull();
+  });
+
+  it('should not work if dividing by zero', () => {
+    let num1 = randomNum;
+    let num2 = 0;
+    let results = arithmetic.divide(num1, num2);
+    expect(results).toBeUndefined();
+  })
 })
